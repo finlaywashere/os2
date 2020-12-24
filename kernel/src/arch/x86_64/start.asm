b5a800bce4cd6b5f8f21eef718d827a17a6e7555
finlaywashere <finman292004@protonmail.com>
2020-12-24 13:54:26 -0500

    Got kernel C code successfully running!

--- kernel/src/arch/x86_64/start.asm
+++ kernel/src/arch/x86_64/start.asm
@@ -1,14 +1,33 @@
 bits 64
 
-;section .bss
-;align 8
-;stack_bottom:
-;resb 0x10000 ; 1MiB
-;stack_top:
+section .bss
+align 8
+stack_bottom:
+resb 0x10000 ; 1MiB
+stack_top:
 
 section .text
 global _start
 _start:
-	mov rax, 0xB8000 ; Move video memory to rax
-	mov byte [rax], 'A' 
-	jmp $
+	pop rbx
+	mov rsp, stack_top ; Setup stack
+	push rbx
+	
+	mov ax, 16
+	
+	; Setup segments
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
+	mov ax, 0
+	
+	extern kernel_start
+	call kernel_start
+	
+	; Something terrible has happened
+	cli
+.hang: hlt
+	jmp .hang
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,3 +1,33 @@
+#include <stdint.h>
+#include <stddef.h>
+
+static const uint16_t *VGA_MEMORY = (uint16_t*) 0xB8000;
+static const VGA_WIDTH = 80;
+static const VGA_HEIGHT = 25;
+
+uint16_t* terminal_buffer;
+
+size_t terminal_row, terminal_col;
+
+void terminal_init(){
+	terminal_buffer = VGA_MEMORY;
+	terminal_row = 0;
+	terminal_col = 0;
+}
+void terminal_putchar(char c){
+	unsigned char uc = c;
+	size_t index = terminal_row * VGA_WIDTH + terminal_col;
+	terminal_buffer[index] = ((uint16_t)uc) | (7 | 0<<4) << 8;
+	terminal_col++;
+}
+
+void terminal_write(const char* data, size_t size){
+	for(size_t i = 0; i < size; i++){
+		terminal_putchar(data[i]);
+	}
+}
+
 void kernel_start(){
-	
+	terminal_init();
+	terminal_write("Successfully booted kernel!",28);
 }

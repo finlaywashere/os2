0b16c6f63386194b62ca68d355204a4a85ff3647
finlaywashere <finman292004@protonmail.com>
2020-12-27 02:20:20 -0500

    Made the kernel use multiple tty's that it switches between

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -5,9 +5,22 @@
 
 void kernel_start(){
 	init_kmalloc(1024*100); // Assume 100MiB of memory
-	init_ttys(1);
-	tty_writestring(0,"Successfully booted kernel!");
+	init_ttys(3);
+	tty_writestring(2,"Successfully booted kernel!");
+	tty_writestring(1,"Test1234");
+	uint64_t i = 0;
+	int mode = 0;
 	while(1){
-		
+		i++;
+		if(i >= 0xFFFFFFFFFFFFFFFF){
+			i = 0;
+			if(mode){
+				tty_copy(1,0);
+				mode = 0;
+			}else{
+				tty_copy(2,0);
+				mode = 1;
+			}
+		}
 	}
 }

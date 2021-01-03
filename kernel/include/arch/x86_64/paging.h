f378e35d5385c1c85968f7026806e4686ad7b92c
finlaywashere <finman292004@protonmail.com>
2021-01-02 23:15:44 -0500

    Added struct for page tables

--- kernel/include/arch/x86_64/paging.h
+++ kernel/include/arch/x86_64/paging.h
@@ -3,8 +3,13 @@
 #include <stdint.h>
 #include <stddef.h>
 
+struct page_table{
+	uint64_t entries[512];
+}__attribute__((packed));
+typedef struct page_table page_table_t;
+
 void init_paging();
-uint64_t* get_page_directory();
+page_table_t* get_page_directory();
 void flush_tlb(uint64_t addr);
 uint64_t get_physical_addr(uint64_t virtual);
 

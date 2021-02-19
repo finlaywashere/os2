7be05d4366eeecfe591e562c9943ab983e4466dc
finlaywashere <finman292004@protonmail.com>
2021-02-18 20:00:43 -0500

    Got TSS working

--- kernel/src/arch/x86_64/gdt.c
+++ kernel/src/arch/x86_64/gdt.c
@@ -1,18 +1,27 @@
 #include <arch/x86_64/gdt.h>
 
 gdt_entry_t* gdt;
+tss_t* tss;
 
 gdt_ptr_t gdt_ptr;
 
 void init_gdt(){
-	gdt = (gdt_entry_t*) kmalloc_p(sizeof(gdt_entry_t)*3);
+	gdt = (gdt_entry_t*) kmalloc_p(sizeof(gdt_entry_t)*7);
+	tss = (tss_t*) kmalloc_p(sizeof(tss_t));
 	
 	set_gdt_entry(0,0,0,0,0); // Null segment
 	set_gdt_entry(1,0,0xFFFFFFFF,0b10011000, 0b10100000); // Kernel code segment
 	set_gdt_entry(2,0,0xFFFFFFFF,0b10010010, 0b10000000); // Kernel data segment
+	set_gdt_entry(3,0,0xFFFFFFFF,0b11111000, 0b10100000); // User code segment
+	set_gdt_entry(4,0,0xFFFFFFFF,0b11110010, 0b10000000); // User data segment
+	
+	uint64_t tss_ptr = (uint64_t) tss;
+	
+	set_gdt_entry(5,(uint32_t) tss_ptr,104,0x89,0x40); // TSS low stuff
+	set_gdt_entry(6,(uint32_t) ((uint16_t) (tss_ptr >> 48)),(uint32_t)((uint16_t) (tss_ptr >> 32)),0,0); // TSS high stuff
 	
 	gdt_ptr.base = (uint64_t) gdt;
-        gdt_ptr.limit = sizeof(gdt_entry_t)*3-1;
+        gdt_ptr.limit = sizeof(gdt_entry_t)*7-1;
         load_gdt();
 }
 void set_gdt_entry(int index, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags){
--- kernel/src/arch/x86_64/gdt_asm.asm
+++ kernel/src/arch/x86_64/gdt_asm.asm
@@ -2,4 +2,8 @@ extern gdt_ptr
 global load_gdt
 load_gdt:
         lgdt [gdt_ptr]
+	push rax
+	mov rax, 0x28
+	ltr ax
+	pop rax
         ret

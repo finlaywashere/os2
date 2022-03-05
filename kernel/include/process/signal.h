e6066490e55bacaa5857b69d24195e9811baa252
finlaywashere <finman292004@protonmail.com>
2022-03-04 19:50:42 -0500

    Added signal codes to signal.h

--- kernel/include/process/signal.h
+++ kernel/include/process/signal.h
@@ -4,6 +4,14 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#define SIGNAL_SIGHUP 1
+#define SIGNAL_SIGINT 2
+#define SIGNAL_SIGKILL 3
+#define SIGNAL_SIGALARM 4
+#define SIGNAL_SIGTERM 5
+#define SIGNAL_SIGTRAP 6
+#define SIGNAL_SIGABRT 7
+
 int register_signal(uint64_t address, uint64_t index, uint64_t flags, uint64_t condition);
 int get_signal(uint64_t flags);
 int get_error_signal();
